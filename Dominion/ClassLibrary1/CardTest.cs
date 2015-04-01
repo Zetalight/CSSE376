75f552cbe0cd9ea1b1fd62a5283f994fd0726ed6
lanejn <lanejn@rose-hulman.edu>
2015-04-01 14:36:03 -0400

    Made a CardTest for cards

---
+++ Dominion/ClassLibrary1/CardTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1
+{
+    [TestClass()]
+    class CardTest
+    {
+        [TestMethod()]
+        public void testCost()
+        {
+
+        }
+
+        [TestMethod()]
+        public void testName()
+        {
+
+        }
+
+        [TestMethod()]
+        public void testImage()
+        {
+
+        }
+    }
+}
