e77465844b99d7c0bf4da03b8612fb9a926da4f9
lanejn <lanejn@rose-hulman.edu>
2015-04-01 14:53:36 -0400

    Added content to CardTest

--- Dominion/ClassLibrary1/CardTest.cs
+++ Dominion/ClassLibrary1/CardTest.cs
@@ -10,21 +10,35 @@ namespace ClassLibrary1
     [TestClass()]
     class CardTest
     {
+        private int T; //TODO: Change these to Cards
+        private int V;
+        private int K;
+
+        [TestInitialize()]
+        public void setupTests()
+        {
+            this.T = 1; //TODO: Replace these with new Treasure, Victory, and Kingdom Cards
+            this.V = 2;
+            this.K = 3;
+        }
+
         [TestMethod()]
         public void testCost()
         {
-
+            Assert.Fail("No test yet!");
         }
 
         [TestMethod()]
         public void testName()
         {
+            Assert.Fail("No test yet!");
 
         }
 
         [TestMethod()]
         public void testImage()
         {
+            Assert.Fail("No test yet!");
 
         }
     }
