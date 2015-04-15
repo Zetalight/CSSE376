e455ec3ae05a4c75b7381786e810c0b24c0185b3
siscodr <siscodr@rose-hulman.edu>
2015-04-15 11:17:28 -0400

    ActionCard Adventure Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionAdventure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionAdventure : ActionCard
+    {
+        public ActionAdventure()
+        {
+            this.name = "Adventure";
+            this.cost = 6;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Adventure Played");
+        }
+    }
+}
