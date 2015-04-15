86e612887d70a8fab4e93106b55210d845d6bf70
siscodr <siscodr@rose-hulman.edu>
2015-04-15 07:36:37 -0400

    Added ActionCards Test for Moat and Names

--- Dominion/ClassLibrary1/ActionCardTest.cs
+++ Dominion/ClassLibrary1/ActionCardTest.cs
@@ -20,25 +20,33 @@ namespace ClassLibrary1
             A = new ActionVillage();
             A = new ActionWitch();
             A = new ActionCellar();
+            A = new ActionMoat();
         }
         [TestMethod()]
-        public void TestActionCardsCost()
+        public void TestActionCardsValues()
         {
             //Costs 2
             A = new ActionChapel();
             Assert.AreEqual(2, A.Cost);
+            Assert.AreEqual("Chapel", A.Name);
             A = new ActionCellar();
             Assert.AreEqual(2, A.Cost);
+            Assert.AreEqual("Cellar", A.Name);
+            A = new ActionMoat();
+            Assert.AreEqual(2, A.Cost);
+            Assert.AreEqual("Moat", A.Name);
 
             //Costs 3
             A = new ActionVillage();
             Assert.AreEqual(3, A.Cost);
+            Assert.AreEqual("Village", A.Name);
 
             //Costs 4
 
             //Costs 5
             A = new ActionWitch();
             Assert.AreEqual(5, A.Cost);
+            Assert.AreEqual("Witch", A.Name);
 
             //Costs 6
         }
