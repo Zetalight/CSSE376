8a821be2feba0f2adc7592d63500f313f88f5d96
siscodr <siscodr@rose-hulman.edu>
2015-04-15 07:32:30 -0400

    Added ActionCard Cellar to pass Test
    Cellar Added

---
+++ Dominion/DominionSharp/ActionCellar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionCellar : ActionCard
+    {
+        public ActionCellar()
+        {
+            this.name = "Cellar";
+            this.cost = 2;
+            this.picture = null; //TODO: Add picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Cellar Played");
+        }
+    }
+}
