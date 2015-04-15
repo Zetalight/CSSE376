7e12c16fa75b9a3b5bd87a29ab492abcf9e428fc
siscodr <siscodr@rose-hulman.edu>
2015-04-15 11:15:23 -0400

    ActionCard Mine Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionMine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionMine : ActionCard
+    {
+        public ActionMine()
+        {
+            this.name = "Mine";
+            this.cost = 5;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Mine Played");
+        }
+    }
+}
