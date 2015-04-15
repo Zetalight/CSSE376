be5ba7b6f11f3a4a8a23221cf8d8ccf1c927600b
siscodr <siscodr@rose-hulman.edu>
2015-04-15 07:57:22 -0400

    ActionCard Workshop Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionWorkshop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionWorkshop : ActionCard
+    {
+        public ActionWorkshop()
+        {
+            this.name = "Workshop";
+            this.cost = 3;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Workshop Played");
+        }
+    }
+}
