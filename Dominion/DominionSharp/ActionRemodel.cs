04c4704e7b1a1e2ad3fd42449d2e208aac93352d
siscodr <siscodr@rose-hulman.edu>
2015-04-15 10:47:46 -0400

    ActionCard Remodel Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionRemodel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionRemodel : ActionCard
+    {
+        public ActionRemodel()
+        {
+            this.name = "Remodel";
+            this.cost = 4;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Remodel Played");
+        }
+    }
+}
