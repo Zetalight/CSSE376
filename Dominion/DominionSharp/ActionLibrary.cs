ae83d825ea0fab7298d8ab64f11e67c70b26c888
siscodr <siscodr@rose-hulman.edu>
2015-04-15 11:11:13 -0400

    ActionCard Library Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionLibrary : ActionCard
+    {
+        public ActionLibrary()
+        {
+            this.name = "Library";
+            this.cost = 5;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Library Played");
+        }
+    }
+}
