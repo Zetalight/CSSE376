9c458bb96577da49b32acee1b95475a0faeaa60a
siscodr <siscodr@rose-hulman.edu>
2015-04-15 11:13:31 -0400

    ActionCard Market Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionMarket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionMarket : ActionCard
+    {
+        public ActionMarket()
+        {
+            this.name = "Market";
+            this.cost = 5;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Market Played");
+        }
+    }
+}
