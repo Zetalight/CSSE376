da1566000bddb641a9ff8c6cee0e0814abc90285
siscodr <siscodr@rose-hulman.edu>
2015-04-15 07:53:58 -0400

    ActionCard Woodcutter Added to pass ActionCard Tests

---
+++ Dominion/DominionSharp/ActionWoodcutter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionWoodcutter : ActionCard
+    {
+        public ActionWoodcutter()
+        {
+            this.name = "Woodcutter";
+            this.cost = 3;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Woodcutter Played");
+        }
+    }
+}
