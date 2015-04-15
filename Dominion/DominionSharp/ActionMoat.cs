8ec7563ef9e10b38553795a48223316716d0b2f4
siscodr <siscodr@rose-hulman.edu>
2015-04-15 07:38:34 -0400

    Added ActionCard Moat to pass ActionCard Tests

---
+++ Dominion/DominionSharp/ActionMoat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionMoat : ActionCard
+    {
+        public ActionMoat()
+        {
+            this.name = "Moat";
+            this.cost = 2;
+            this.picture = null ; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Moat Played");
+        }
+    }
+}
