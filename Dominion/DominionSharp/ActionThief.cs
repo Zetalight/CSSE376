22916d1542397d10412ba94189287d86af8f1ee7
siscodr <siscodr@rose-hulman.edu>
2015-04-15 10:55:22 -0400

    ActionCard Thief Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionThief.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionThief : ActionCard
+    {
+        public ActionThief()
+        {
+            this.name = "Thief";
+            this.cost = 4;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Thief Played");
+        }
+    }
+}
