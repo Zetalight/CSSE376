29a319837121041639a82e3d102f38a971cc98cb
siscodr <siscodr@rose-hulman.edu>
2015-04-15 11:02:43 -0400

    ActionCard Council Room Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/CouncilRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionCouncilRoom : ActionCard
+    {
+        public ActionCouncilRoom()
+        {
+            this.name = "Council Room";
+            this.cost = 5;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Council Room Played");
+        }
+    }
+}
