753a2c87bb28a3d129ca7f41409d9488b6b3c5c7
lanejn <lanejn@rose-hulman.edu>
2015-04-15 14:15:45 -0400

    New tests
    empty test for action functions, playertest

---
+++ Dominion/ClassLibrary1/PlayerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1
+{
+    [TestClass()]
+    public class PlayerTest
+    {
+        [TestMethod()]
+        public void TestThatPlayerInitializes()
+        {
+            Player p = new Player();
+        }
+    }
+}
