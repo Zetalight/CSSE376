71fa6efb3a0f096100fb203143254eff79b4120e
siscodr <siscodr@rose-hulman.edu>
2015-04-15 07:24:05 -0400

    Added more ActionCard Tests
    Started a separate ActionCard Tests to a new test due to volume of
    Action cards to be implemented.

---
+++ Dominion/ClassLibrary1/ActionCardTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominionSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1
+{
+    [TestClass()]
+    public class ActionCards
+    {
+        private ActionCard A;
+
+        [TestMethod()]
+        public void TestThatActionCardsCanInitialize()
+        {
+            A = new ActionChapel();
+            A = new ActionVillage();
+            A = new ActionWitch();
+            A = new ActionCellar();
+        }
+        [TestMethod()]
+        public void TestActionCardsCost()
+        {
+            //Costs 2
+            A = new ActionChapel();
+            Assert.AreEqual(2, A.Cost);
+            A = new ActionCellar();
+            Assert.AreEqual(2, A.Cost);
+
+            //Costs 3
+            A = new ActionVillage();
+            Assert.AreEqual(3, A.Cost);
+
+            //Costs 4
+
+            //Costs 5
+            A = new ActionWitch();
+            Assert.AreEqual(5, A.Cost);
+
+            //Costs 6
+        }
+    }
+}
