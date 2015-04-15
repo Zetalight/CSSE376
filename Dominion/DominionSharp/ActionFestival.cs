8b3930b43e980a1b07a61b33568cf95437cf5b13
siscodr <siscodr@rose-hulman.edu>
2015-04-15 11:06:08 -0400

    ActionCard Festival Added to pass ActionCard Test
    -Changed name of Council Room to appropriately fit other ActionCard
    names

---
+++ Dominion/DominionSharp/ActionFestival.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionFestival : ActionCard
+    {
+        public ActionFestival()
+        {
+            this.name = "Festival";
+            this.cost = 5;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Festival Played");
+        }
+    }
+}
