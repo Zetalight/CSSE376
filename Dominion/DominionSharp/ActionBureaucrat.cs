ffbd62d4d01bc30200d059efe93d285fad67f976
siscodr <siscodr@rose-hulman.edu>
2015-04-15 08:00:21 -0400

    ActionCard Bureaucrat Added to pass ActionCard Test

---
+++ Dominion/DominionSharp/ActionBureaucrat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    public class ActionBureaucrat : ActionCard
+    {
+        public ActionBureaucrat()
+        {
+            this.name = "Bureaucrat";
+            this.cost = 4;
+            this.picture = null; //TODO: Add Picture
+        }
+        public override void play()
+        {
+            Console.WriteLine("Bureaucrat Played");
+        }
+    }
+}
