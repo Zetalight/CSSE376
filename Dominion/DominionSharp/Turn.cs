37aa8e71f6acda5a36deb377745b8412064b90a4
lanejn <lanejn@rose-hulman.edu>
2015-04-22 16:15:38 -0400

    turn works fine for now (no phases)

--- Dominion/DominionSharp/Turn.cs
+++ Dominion/DominionSharp/Turn.cs
@@ -21,36 +21,67 @@ namespace DominionSharp
             this.players = plist;
             this.currentPlayerIndex = 0;
             activePlayer = plist[0];
-            this.coins = 0;
-            this.buys = 1;
-            this.actions = 1;
-            this.trashes = 0;
+            this.reInitVals();
         }
 
         public int getActions()
         {
             return this.actions;
         }
+
+        public void setActions(int i)
+        {
+            this.actions = i;
+        }
+
         public int getBuys()
         {
             return this.buys;
         }
+
+        public void setBuys(int i)
+        {
+            this.buys = i;
+        }
+
         public int getCoins()
         {
             return this.coins;
         }
+
+        public void setCoins(int i)
+        {
+            this.coins = i;
+        }
+
         public int getTrashes()
         {
             return this.trashes;
         }
+
+        public void setTrashes(int i)
+        {
+            this.trashes = i;
+        }
+
         public Player getActivePlayer()
         {
             return this.activePlayer;
         }
+
         public void nextTurn()
         {
             this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.Count;
             this.activePlayer = this.players[this.currentPlayerIndex];
+            this.reInitVals();
+        }
+
+        public void reInitVals()
+        {
+            this.coins = 0;
+            this.buys = 1;
+            this.actions = 1;
+            this.trashes = 0;
         }
     }
 }
