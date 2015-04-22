80b525d5a14b520fbbf8c0a5f786cf3d8a118804
lanejn <lanejn@rose-hulman.edu>
2015-04-22 14:21:35 -0400

    tests for player

--- Dominion/ClassLibrary1/PlayerTest.cs
+++ Dominion/ClassLibrary1/PlayerTest.cs
@@ -32,6 +32,8 @@ namespace ClassLibrary1
             Assert.AreEqual(0, p.getCoins());
             Assert.AreEqual(1, p.getActions());
             Assert.AreEqual(1, p.getBuys());
+            Assert.AreEqual(5, p.getDeckSize());
+            Assert.AreEqual(5, p.getHandSize());
             p.setCoins(2);
             p.setActions(2);
             p.setBuys(2);
