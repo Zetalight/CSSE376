4681e96e4875f8bed6f7f70bea7286516cbeec96
lanejn <lanejn@rose-hulman.edu>
2015-05-06 15:21:56 -0400

    supply test now runs for 2, 3, 4 players

--- Dominion/ClassLibrary1/GameInitializationTest.cs
+++ Dominion/ClassLibrary1/GameInitializationTest.cs
@@ -66,28 +66,32 @@ namespace ClassLibrary1
         [TestMethod()]
         public void TestThatGameMakesSupply()
         {
-            List<Pile> p = game.getPiles();
-            Assert.IsNotNull(p);
-            Assert.AreEqual(10, p.Count());
-            List<Pile> t = game.getTreasures();
-            Assert.IsNotNull(t);
-            Assert.AreEqual(3, t.Count());
-            Assert.AreEqual(60 - (7 * Turn.Instance.Players.Count()), t[0].getCount());
-            Assert.AreEqual(typeof(TreasureCopper), t[0].getCard().GetType());
-            Assert.AreEqual(40, t[1].getCount());
-            Assert.AreEqual(typeof(TreasureSilver), t[1].getCard().GetType());
-            Assert.AreEqual(30, t[2].getCount());
-            Assert.AreEqual(typeof(TreasureGold), t[2].getCard().GetType());
-            List<Pile> v = game.getVictories();
-            Assert.IsNotNull(v);
-            Assert.AreEqual(Turn.Instance.Players.Count() == 2 ? 8 : 12, v[0].getCount());
-            Assert.AreEqual(typeof(VictoryEstate), v[0].getCard().GetType());
-            Assert.AreEqual(v[0].getCount(), v[1].getCount());
-            Assert.AreEqual(typeof(VictoryDuchy), v[1].getCard().GetType());
-            Assert.AreEqual(v[0].getCount(), v[2].getCount());
-            Assert.AreEqual(typeof(VictoryProvince), v[2].getCard().GetType());
-            Assert.AreEqual(10 * (Turn.Instance.Players.Count() - 1), v[3].getCount());
-            Assert.AreEqual(typeof(VictoryCurse), v[3].getCard().GetType());
+            for (int n = 2; n <= 4; n++)
+            {
+                game.setPlayerCount(n);
+                List<Pile> p = game.getPiles();
+                Assert.IsNotNull(p);
+                Assert.AreEqual(10, p.Count());
+                List<Pile> t = game.getTreasures();
+                Assert.IsNotNull(t);
+                Assert.AreEqual(3, t.Count());
+                Assert.AreEqual(60 - (7 * n), t[0].getCount());
+                Assert.AreEqual(typeof(TreasureCopper), t[0].getCard().GetType());
+                Assert.AreEqual(40, t[1].getCount());
+                Assert.AreEqual(typeof(TreasureSilver), t[1].getCard().GetType());
+                Assert.AreEqual(30, t[2].getCount());
+                Assert.AreEqual(typeof(TreasureGold), t[2].getCard().GetType());
+                List<Pile> v = game.getVictories();
+                Assert.IsNotNull(v);
+                Assert.AreEqual(n == 2 ? 8 : 12, v[0].getCount());
+                Assert.AreEqual(typeof(VictoryEstate), v[0].getCard().GetType());
+                Assert.AreEqual(v[0].getCount(), v[1].getCount());
+                Assert.AreEqual(typeof(VictoryDuchy), v[1].getCard().GetType());
+                Assert.AreEqual(v[0].getCount(), v[2].getCount());
+                Assert.AreEqual(typeof(VictoryProvince), v[2].getCard().GetType());
+                Assert.AreEqual(10 * (n - 1), v[3].getCount());
+                Assert.AreEqual(typeof(VictoryCurse), v[3].getCard().GetType());
+            }
         }
     }
 }
