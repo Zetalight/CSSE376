301898b841b12dc8bfa3a9b2af8e700f55390b4f
lanejn <lanejn@rose-hulman.edu>
2015-05-06 15:17:25 -0400

    expanded supply tests

--- Dominion/ClassLibrary1/GameInitializationTest.cs
+++ Dominion/ClassLibrary1/GameInitializationTest.cs
@@ -73,13 +73,19 @@ namespace ClassLibrary1
             Assert.IsNotNull(t);
             Assert.AreEqual(3, t.Count());
             Assert.AreEqual(60 - (7 * Turn.Instance.Players.Count()), t[0].getCount());
+            Assert.AreEqual(typeof(TreasureCopper), t[0].getCard().GetType());
             Assert.AreEqual(40, t[1].getCount());
+            Assert.AreEqual(typeof(TreasureSilver), t[1].getCard().GetType());
             Assert.AreEqual(30, t[2].getCount());
+            Assert.AreEqual(typeof(TreasureGold), t[2].getCard().GetType());
             List<Pile> v = game.getVictories();
             Assert.IsNotNull(v);
             Assert.AreEqual(Turn.Instance.Players.Count() == 2 ? 8 : 12, v[0].getCount());
+            Assert.AreEqual(typeof(VictoryEstate), v[0].getCard().GetType());
             Assert.AreEqual(v[0].getCount(), v[1].getCount());
+            Assert.AreEqual(typeof(VictoryDuchy), v[1].getCard().GetType());
             Assert.AreEqual(v[0].getCount(), v[2].getCount());
+            Assert.AreEqual(typeof(VictoryProvince), v[2].getCard().GetType());
         }
     }
 }
