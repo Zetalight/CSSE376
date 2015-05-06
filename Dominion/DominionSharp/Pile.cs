cfec9c6d1402611b5236f82d625e07d5e2220e87
siscodr <siscodr@rose-hulman.edu>
2015-05-06 14:34:55 -0400

    Added Pile

---
+++ Dominion/DominionSharp/Pile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominionSharp
+{
+    class Pile
+    {
+        private Card card;
+        private int count;
+
+        public Pile(Card card, int count)
+        {
+            this.card = card;
+            this.count = count;
+        }
+        public Pile(Card card)
+        {
+            this.card = card;
+            this.count = 10;
+        }
+        private void setCard(Card card){
+            this.card = card;
+        }
+        private Card getCard()
+        {
+            return this.card;
+        }
+        private void setCount(int count)
+        {
+            this.count = count;
+        }
+        private int getCount()
+        {
+            return this.count;
+        }
+        private void draw()
+        {
+            count--;
+        }
+    }
+}
