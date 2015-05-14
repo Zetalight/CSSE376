9a11154237c277bf2e479d16e2fd95e8dd586deb
siscodr <siscodr@rose-hulman.edu>
2015-05-14 19:52:06 -0400

    Added Card back

--- Dominion/DominionSharp/Properties/Resources.Designer.cs
+++ Dominion/DominionSharp/Properties/Resources.Designer.cs
@@ -379,13 +379,5 @@ namespace DominionSharp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        internal static System.Drawing.Bitmap garden
-        {
-            get
-            {
-                object obj = ResourceManager.GetObject("garden", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
