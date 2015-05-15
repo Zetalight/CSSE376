628a663ea31568589e588c461b25a5f997f078be
siscodr <siscodr@rose-hulman.edu>
2015-05-14 20:00:45 -0400

    Fixed-Inputing back

--- Dominion/DominionSharp/Properties/Resources.Designer.cs
+++ Dominion/DominionSharp/Properties/Resources.Designer.cs
@@ -379,5 +379,30 @@ namespace DominionSharp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap garden
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("garden", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap back
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
     }
 }
