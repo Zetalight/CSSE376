26d802fb175c0397ffb22857be089125a1e11713
lanejn <lanejn@rose-hulman.edu>
2015-05-20 14:28:27 -0400

    Fixed players decks not starting shuffled

--- Dominion/DominionSharp/Player.cs
+++ Dominion/DominionSharp/Player.cs
@@ -13,33 +13,14 @@ namespace DominionSharp
         private List<Card> deck = new List<Card>();
         private List<Card> hand = new List<Card>();
         private List<Card> discard = new List<Card>();
-        private int number = -1;
+        private int number;
         private bool hidden = true;
         
         // list of cards that have been played for the 
         // current turn, but not yet discarded
         private List<Card> wumbo = new List<Card>();
 
-        public Player()
-        {
-            // set initial deck cards (7 coppers, 3 estates)
-            for (int i = 0; i < 7; i++)
-            {
-                deck.Add(new TreasureCopper());
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                deck.Add(new VictoryEstate());
-            }
-
-            // draw initial hand of 5 cards
-            for (int i = 0; i < 5; i++)
-            {
-                drawCard();
-            }
-        }
-
-        public Player(int number)
+        public Player(int number = -1)
         {
             this.number = number;
             // set initial deck cards (7 coppers, 3 estates)
@@ -47,10 +28,13 @@ namespace DominionSharp
             {
                 deck.Add(new TreasureCopper());
             }
+
             for (int i = 0; i < 3; i++)
             {
                 deck.Add(new VictoryEstate());
             }
+
+            shuffleDeck();
 
             // draw initial hand of 5 cards
             for (int i = 0; i < 5; i++)
