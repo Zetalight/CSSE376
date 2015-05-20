028816c8535d2e63e92f1762020d4db7ba526e00
siscodr <siscodr@rose-hulman.edu>
2015-05-20 14:22:43 -0400

    Added play function for Chancellor
    Test for chancellor passes

--- Dominion/DominionSharp/ActionCards/ActionChancellor.cs
+++ Dominion/DominionSharp/ActionCards/ActionChancellor.cs
@@ -20,6 +20,15 @@ namespace DominionSharp
             base.play();
             Console.WriteLine("Chancellor Played");
             //TODO: You may immediately put your deck into your discard pile.
+            Player current = Turn.Instance.getActivePlayer();
+            List<Card> newDiscard = current.getDiscard();
+            newDiscard.AddRange(current.getDeck());
+            typeof(Player)
+                .GetField("discard", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                .SetValue(Turn.Instance.getActivePlayer(), newDiscard);
+            typeof(Player)
+                .GetField("deck", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                .SetValue(Turn.Instance.getActivePlayer(), new List<Card>());
         }
     }
 }
